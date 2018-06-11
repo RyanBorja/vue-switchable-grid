2ee3f4bd193db71683f9066c67322c692bcd8513
Janine Hempy <jhempy@gmail.com>
2018-06-11 11:21:20 -0400

    Completes first draft of assignment

--- main.js
+++ main.js
@@ -4,13 +4,33 @@ var demo = new Vue({
 
 	data: {
 
-		layout: 'grid'
+		layout: 'grid',
 
+		results: []
 
 	},
 
-	methods: {
+	mounted: function() {
 
+			var self = this;
+			var url = "http://thecatapi.com/api/images/get?format=xml&results_per_page=6";
+
+			axios.get(url)
+				.then(function(response) {
+
+					var parser = new DOMParser(); 
+					var data = parser.parseFromString(response.data,"text/xml");
+
+					// console.log(data);
+
+					Array.from(data.getElementsByTagName('url'))
+					.forEach(function(image_url, index) {
+						// console.log("index is " + index);
+						// console.log(image_url.innerHTML);
+						self.results.push(image_url.innerHTML);
+					});
+
+				});
 
 	}
 
